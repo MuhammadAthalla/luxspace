416bbf1668260490b456d8cb60e6f34f59b112ca
MuhammadAthalla <juraganathalla@gmail.com>
2023-05-31 08:00:43 +0700

    udah ke fix coyy 🍆💦

--- resources/views/pages/dashboard/product/index.blade.php
+++ resources/views/pages/dashboard/product/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('product') }}
+            {{ __('Products') }}
         </h2>
     </x-slot>
 
@@ -43,29 +43,23 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="mb-10">
-                <a href="{{route('dashboard.product.create')}}" class="bg-green-500 py-2 px-4 rounded shadow-lg hover:bg-green-700 text-white font-bold ">+ Create Data</a>
+                <a href="{{ Route('dashboard.product.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded shadow-lg">+ Create Data</a>
             </div>
             <div class="shadow overflow-hidden sm-rounded-md">
-                <div class="px-4 py-5 bg-white sm:p-6 ">
+                <div class="px-4 py-5 bg-white sm:p-6">
                     <table id="crudTable">
                         <thead>
-                            <th>
-                                <tr>ID</tr>
-                                <tr>nama</tr>
-                                <tr>price</tr>
-                                <tr>qty</tr>
-                                <tr>Aksi</tr>
-                            </th>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nama</th>
+                                <th>Harga</th>
+                                <th>QTY</th>
+                                <th>Aksi</th>
+                            </tr>
                         </thead>
                     </table>
                 </div>
             </div>
         </div>
     </div>
-
-
-
-
-    </div>
-
 </x-app-layout>
